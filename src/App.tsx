adc643104de4a79e14f5254761e540555ca3fad8
Luc Hoang <luc.hn@teko.vn>
2021-07-25 21:11:08 +0700

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import { Row, Col } from "antd";
 import "antd/dist/antd.css";
 import "./App.css";
@@ -15,46 +13,6 @@ const Wrapper = styled.div`
   bottom: 10%;
   left: 0;
   right: 0;
-`;
-
-const Content = styled.div`
-  display: flex;
-  gap: 16px;
-  align-items: center;
-  justify-content: center;
-`;
-
-const TableWrapper = styled.div`
-  background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(4px);
-  -webkit-backdrop-filter: blur(4px);
-  border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.18);
-  width: 100%;
-  text-align: center;
-  color: #fff;
-  padding: 8px 12px;
-`;
-
-const Divider = styled.div`
-  height: 1px;
-  width: 100%;
-  background: #fff;
-  margin: 8px 0;
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 8fr 3fr 3fr 3fr;
-  row-gap: 8px;
-  div {
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 1;
-    overflow: hidden;
-  }
 `;
 
 function App() {
